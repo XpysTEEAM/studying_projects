79d8750e20db38af11a6c9986fa656c10859089b
Jenya <nikolaev-evgenij@bk.ru>
2021-05-11 14:56:05 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+"use strict";
+
+let number = 88.3;
+
+const person = "Alex";
+
+const bool = true;
+
+const obj = {
+   name: "jeny",
+   age: 22,
+   isMarried: false
+};
+
+console.log(obj["name"]);
+
+let arr = [1, 4, 6, 4];
+
+console.log(arr[2]);
+
+//const result = confirm("person");
+//console.log(result);
+
+// const ans = prompt("are you 18?", "da");
+// console.log(typeof (ans));
+
+const answers = [];
+
+answers[0] = prompt("ты кто?", "");
+answers[1] = prompt("ты что?", "");
+answers[2] = prompt("ты как?", "");
+
+document.write(answers);
+
+//интерполяция
+
+const category = "games";
+
+console.log(`htpps://url.com/${category}/5`);
+
+const user = "Jeny"
+
+alert(`Здравствуй, ${answers[0]}`)
+
+//инкримент и декримент
+
+let incr = 10,
+   decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr, decr);
+
+console.log(2 % 1 == 0);
+
+//операторы
+
+! - отрицание;
+&& - "и";
+|| - "или";
